b23f9eeb2eedc032d8f61b14e8d968cf394c3c4c
xyrlan <lobatopedro68@gmail.com>
2024-02-29 11:22:10 -0400

    Refactor FotosVideosPage component layout

--- src/app/fotos-e-videos/page.tsx
+++ src/app/fotos-e-videos/page.tsx
@@ -5,8 +5,9 @@ import React from 'react'
 
 const FotosVideosPage = () => {
   return (
-    <div className="grid min-h-screen place-content-center bg-white px-4 py-52">
-      <div className="text-center flex flex-col md:flex-row  items-center md:gap-20">
+    <div className="grid min-h-screen place-content-center bg-white px-4 py-52 text-center ">
+      <h1 className='text-3xl md:text-5xl font-semibold text-primary'>Fotos e Videos</h1>
+      <div className="text-center flex justify-center items-center md:gap-20">
         <Image src={'/images/fisherman.jpg'} width={500} height={500} alt='404' className='w-auto max-h-[300px]' />
       </div>
       <Button className='mt-8'><Link href={'/fotos'}>Acessar fotos do Pesqueiro 100</Link></Button>
