fe8f3b2819a15e79d3cc9af1b85c9720fe019079
xyrlan <lobatopedro68@gmail.com>
2024-02-29 11:18:33 -0400

    Refactor code to improve performance and readability

---
+++ src/app/fotos-e-videos/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const FotosVideosPage = () => {
+  return (
+    <div className="grid min-h-screen place-content-center bg-white px-4 py-52">
+      <div className="text-center flex flex-col md:flex-row  items-center md:gap-20">
+        <Image src={'/images/fisherman.jpg'} width={500} height={500} alt='404' className='w-auto max-h-[300px]' />
+      </div>
+      <Button className='mt-8'><Link href={'/fotos'}>Acessar fotos do Pesqueiro 100</Link></Button>
+      <Button className='mt-8'><Link href={'/videos'}>Acessar videos do Pesqueiro 100</Link></Button>
+    </div>
+  )
+}
+
+export default FotosVideosPage
